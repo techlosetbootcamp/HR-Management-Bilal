8ba71bd7b2c1ebf4e466ecceb3ef6f2b8564cbbf
CodeWithMian <mb03086350364@gmail.com>
2025-02-06 23:38:40 +0500

    TESR-ROUTIING

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+// import Link from 'next/link'
 import React from 'react'
-
+import Login from './auth/login/page'
 function page() {
   return (
     <div className='flex justify-center items-center w-full h-auto text-[200px]'>
-      Home
+      <Login/>
     </div>
   )
 }
